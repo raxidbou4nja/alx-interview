f1a75ae09d3ba6d97f08ea09e2996a1bf59ce4aa
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-04-25 19:03:12 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-""" Minimum Operations """
+""" Minimum Operations
+    """
 
 
 def minOperations(n: int) -> int:
-    """
-    script to calculate the minimum number of
-    operations to reach 'n' characters.
-
-    Args: (n) Integer, the target number of characters.
-
-    Returns: Integer, the minimum number of operations required.
-    """
+    """ Minimum Operations needed to get n H characters """
     next = 'H'
     body = 'H'
     op = 0
