4a7de85a12802d0310f9c57864c3e8af6aec255c
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-05-02 19:51:01 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,43 @@
 #!/usr/bin/python3
 """
-log parsing
+Log Parsing
 """
 
 import sys
 import re
 
 
-def output(log: dict) -> None:
+def display_statistics(log):
     """
-    Helper function to display stats
+    Display file size and status code statistics
     """
-    print("File size: {}".format(log["file_size"]))
+    print("Total file size:", log["file_size"])
     for code in sorted(log["code_frequency"]):
         if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+            print(f"{code}: {log['code_frequency'][code]}")
 
 
 if __name__ == "__main__":
-    regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
 
+    log = {"file_size": 0, "code_frequency": {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}}
     line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
             line = line.strip()
-            match = regex.fullmatch(line)
+            match = regex.match(line)
             if match:
                 line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+                status_code = match.group(2)
+                file_size = int(match.group(3))
 
-                # File size
                 log["file_size"] += file_size
 
-                # status code
-                if code.isdecimal():
-                    log["code_frequency"][code] += 1
+                if status_code in log["code_frequency"]:
+                    log["code_frequency"][status_code] += 1
 
                 if line_count % 10 == 0:
-                    output(log)
+                    display_statistics(log)
     finally:
-        output(log)
+        display_statistics(log)
