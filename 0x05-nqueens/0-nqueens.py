2f6778a3131b13413c166afb5174c466a83bc9a7
root <rachid.boughnja@gmail.com>
2024-05-16 12:30:29 -0700

    add new files

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,73 +1,46 @@
-#!/usr/bin/env python3
-""" Main """
+#!/usr/bin/python3
+"""N queens problem solver."""
 import sys
 
 
-def is_safe(board, row, col):
-    """ Check if it's safe to place a queen at board[row][col] """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solve_nqueens_util(board, col, solutions):
-    """ Solve the N-Queens problem using backtracking """
-    if col >= len(board):
-        solution = []
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        solutions.append(solution)
-        return True
-
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_nqueens_util(board, col + 1, solutions) or res
-            board[i][col] = 0
-
-    return res
-
-
-def solve_nqueens(N):
-    """ Solve the N-Queens problem """
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solutions = []
-    solve_nqueens_util(board, 0, solutions)
-    return solutions
-
-
-def main():
+def validate_args():
+    """Validate command-line arguments."""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
 
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
+    if not sys.argv[1].isdigit():
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
+
+    N = int(sys.argv[1])
 
     if N < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-    solutions = solve_nqueens(N)
-    for solution in solutions:
-        print(solution)
+    return N
+
+
+def queens(N, row=0, columns=[], diag1=[], diag2=[]):
+    """Recursive generator function to find all solutions."""
+    if row == N:
+        yield columns
+    else:
+        for col in range(N):
+            if col not in columns and (row + col) not in diag1 and \
+               (row - col) not in diag2:
+                yield from queens(N, row + 1, columns + [col],
+                                  diag1 + [row + col], diag2 + [row - col])
+
+
+def print_solutions(N):
+    """Generate and print all solutions for the N queens problem."""
+    for solution in queens(N):
+        formatted_solution = [[i, solution[i]] for i in range(N)]
+        print(formatted_solution)
 
 
 if __name__ == "__main__":
-    main()
+    N = validate_args()
+    print_solutions(N)
