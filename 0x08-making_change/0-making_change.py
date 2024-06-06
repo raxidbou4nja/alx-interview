5d2bf2a0b87e68b6fec3c9f0d04021b873a51c83
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-06-06 18:36:45 +0100

    Create 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Making changes Script """
+
+
+def makeChange(coins, total):
+    """ Generate changes
+    """
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
